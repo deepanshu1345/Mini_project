9bed64c70689913bbc81715da78450b913c43415
deepanshu1345 <41050479+deepanshu1345@users.noreply.github.com>
2024-04-07 14:31:07 +0530

    new project

--- main.py
+++ main.py
@@ -9,11 +9,27 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/loginex'
 
 mongo = PyMongo(app)
 
+
 @app.route("/index")
 def index():
     if 'username' in session:
         return "You are logged in as " + session['username']
-    return redirect(url_for('login'))  # Redirect to login page if not logged in
+    return redirect(url_for('login'))
+
+
+@app.route("/admin")
+def mgrdashbord():
+    if 'm_user' in session:
+        return "You are logged in as " + session['m_user']
+    return redirect(url_for('manager_login'))
+
+
+@app.route("/admin")
+def admindashbord():
+    if 'm_user' in session:
+        return "You are logged in as " + session['m_user']
+    return redirect(url_for(''))
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -25,7 +41,8 @@ def login():
                 session['username'] = request.form['username']
                 return redirect(url_for('index'))
         return "Invalid username or password"
-    return render_template("user.html")
+    return render_template("adminlogin.html")
+
 
 @app.route("/", methods=["GET", "POST"])
 def register():
@@ -34,17 +51,81 @@ def register():
         existing_user = users.find_one({'username': request.form['username']})
 
         if existing_user is None:
-            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
-            users.insert_one({'name': request.form['name'],
-                              'username': request.form['username'],
+            hashpass = bcrypt.hashpw(request.form['m_pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert_one({'name': request.form['c_name'],
+                              'username': request.form['m_username'],
                               'password': hashpass,
-                              'email': request.form['email'],
-                              'age': request.form['age']
-                            })
+                              'email': request.form['m_email']
+                              })
             session['username'] = request.form['username']
             return redirect(url_for('index'))
         return "User already exists"
-    return render_template("index.html")
+    return render_template("addadmin.html")
+
+
+@app.route("/manager_login", methods=["GET", "POST"])
+def manager_login():
+    if request.method == "POST":
+        manager = mongo.db.manager
+        username = manager.find_one({'m_user': request.form['m_user']})
+        if username:
+            if bcrypt.checkpw(request.form['m_password'].encode('utf-8'), username['m_password']):
+                session['m_user'] = request.form['m_user']
+                return redirect(url_for('mgrdashbord'))
+        return "Invalid username or password"
+    return render_template("Manager//mgrlogin.html")
+
+
+@app.route("/manager_register", methods=["GET", "POST"])
+def manager_register():
+    if request.method == "POST":
+        manager = mongo.db.manager
+        existing_user = manager.find_one({'m_user': request.form['m_user']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['m_pass'].encode('utf-8'), bcrypt.gensalt())
+            manager.insert_one({'c_name': request.form['c_name'],
+                                'm_user': request.form['m_user'],
+                                'm_password': hashpass,
+                                'm_email': request.form['m_email'],
+                                })
+            session['m_user'] = request.form['m_user']
+            return redirect(url_for('mgrdashbord'))
+        return "User already exists"
+    return render_template("Manager//mgrregister.html")
+
+
+@app.route("/manager_login", methods=["GET", "POST"])
+def admin_login():
+    if request.method == "POST":
+        admin = mongo.db.admin
+        username = admin.find_one({'m_user': request.form['m_user']})
+        if username:
+            if bcrypt.checkpw(request.form['m_password'].encode('utf-8'), username['m_password']):
+                session['m_user'] = request.form['m_user']
+                return redirect(url_for('mgrdashbord'))
+        return "Invalid username or password"
+    return render_template("Admin//adminlogin.html")
+
+
+@app.route("/manager_register", methods=["GET", "POST"])
+def admin_register():
+    if request.method == "POST":
+        admin = mongo.db.admin
+        existing_user = admin.find_one({'admin': request.form['admin']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['a_pass'].encode('utf-8'), bcrypt.gensalt())
+            admin.insert_one({'c_name': request.form['a_name'],
+                              'admin': request.form['admin'],
+                              'a_password': hashpass,
+                              'a_email': request.form['a_email'],
+                              })
+            session['admin'] = request.form['admin']
+            return redirect(url_for('admindashbord'))
+        return "User already exists"
+    return render_template("Admin//addadmin.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost")
